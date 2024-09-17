d61b0ce40e25d89da6913eefdcc2d77168aa1a68
Kevin Axel Tagliaferri <kevinaxeltagliaferri@gmail.com>
2024-09-17 08:38:39 -0300

    Adding test file for polygon class.

---
+++ tests/geometry/test_polygon.py
@@ -0,0 +1,89 @@
+# Local imports.
+from etacad.globals import DRotation
+from etacad.geometry.polygon import Polygon
+from etacad.globals import DRotation
+
+# External imports.
+import pytest
+
+
+@pytest.fixture
+def polygon_clockwise():
+    return Polygon(vertices=[(0, 0), (0, 1), (1, 1), (2, 2), (3, 2), (3, 0)])
+
+
+def test_polygon_clockwise_attributes(polygon_clockwise):
+    assert polygon_clockwise.perimeter == 6.414213562373095
+    assert polygon_clockwise.area == 4.5
+    assert polygon_clockwise.centroid == (1.5, 1.0)
+
+
+def test_polygon_clockwise_get_points_direction_of_rotation(polygon_clockwise):
+    assert polygon_clockwise.get_points_direction_of_rotation() == DRotation.CLOCKWISE
+
+
+def test_polygon_clockwise_get_points_between_index_limits_by_dr(polygon_clockwise):
+    cw_list_1_4 = polygon_clockwise.get_points_between_index_limits_by_dr(index1=1,
+                                                                          index2=4,
+                                                                          direction_rotation=DRotation.CLOCKWISE)
+
+    assert cw_list_1_4 == [(0, 1), (1, 1), (2, 2), (3, 2)]
+
+    cw_list_0_5 = polygon_clockwise.get_points_between_index_limits_by_dr(index1=0,
+                                                                          index2=5,
+                                                                          direction_rotation=DRotation.CLOCKWISE)
+
+    assert cw_list_0_5 == [(0, 0), (0, 1), (1, 1), (2, 2), (3, 2), (3, 0)]
+
+    ccw_list_1_4 = polygon_clockwise.get_points_between_index_limits_by_dr(index1=1,
+                                                                           index2=4,
+                                                                           direction_rotation=DRotation.COUNTERCLOCKWISE)
+
+    assert ccw_list_1_4 == [(0, 1), (0, 0), (3, 0), (3, 2)]
+
+    ccw_list_0_4 = polygon_clockwise.get_points_between_index_limits_by_dr(index1=0,
+                                                                           index2=4,
+                                                                           direction_rotation=DRotation.COUNTERCLOCKWISE)
+
+    assert ccw_list_0_4 == [(0, 0), (3, 0), (3, 2)]
+
+
+@pytest.fixture
+def polygon_counterclockwise():
+    return Polygon(vertices=[(3, 0), (3, 2), (2, 2), (1, 1), (0, 1), (0, 0)])
+
+
+def test_polygon_counterclockwise_attributes(polygon_counterclockwise):
+    assert polygon_counterclockwise.perimeter == 6.414213562373095
+    assert polygon_counterclockwise.area == 4.5
+    assert polygon_counterclockwise.centroid == (1.5, 1.0)
+
+
+def test_polygon_counterclockwise_get_points_direction_of_rotation(polygon_counterclockwise):
+    assert polygon_counterclockwise.get_points_direction_of_rotation() == DRotation.COUNTERCLOCKWISE
+
+
+def test_polygon_counterclockwise_get_points_between_index_limits_by_dr(polygon_counterclockwise):
+    cw_list_1_4 = polygon_counterclockwise.get_points_between_index_limits_by_dr(index1=1,
+                                                                                 index2=4,
+                                                                                 direction_rotation=DRotation.CLOCKWISE)
+
+    assert cw_list_1_4 == [(3, 2), (3, 0), (0, 0), (0, 1)]
+
+    cw_list_0_5 = polygon_counterclockwise.get_points_between_index_limits_by_dr(index1=0,
+                                                                                 index2=5,
+                                                                                 direction_rotation=DRotation.CLOCKWISE)
+
+    assert cw_list_0_5 == [(3, 0), (0, 0)]
+
+    ccw_list_1_4 = polygon_counterclockwise.get_points_between_index_limits_by_dr(index1=1,
+                                                                                  index2=4,
+                                                                                  direction_rotation=DRotation.COUNTERCLOCKWISE)
+
+    assert ccw_list_1_4 == [(3, 2), (2, 2), (1, 1), (0, 1)]
+
+    ccw_list_0_4 = polygon_counterclockwise.get_points_between_index_limits_by_dr(index1=0,
+                                                                                  index2=4,
+                                                                                  direction_rotation=DRotation.COUNTERCLOCKWISE)
+
+    assert ccw_list_0_4 == [(3, 0), (3, 2), (2, 2), (1, 1), (0, 1)]
