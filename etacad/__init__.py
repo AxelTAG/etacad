145e486675446575fa432e5bf107ada60bcb8910
Kevin Axel Tagliaferri <kevinaxeltagliaferri@gmail.com>
2025-05-02 18:49:06 -0300

    Adding attrs to requirements.

--- etacad/__init__.py
+++ etacad/__init__.py
@@ -2,7 +2,7 @@
 
 """A package aimed to simpilfy drawings of structurals elements based on ezdxf library."""
 
-__version__ = "0.0.7"
+__version__ = "0.0.10"
 
 from .bar import Bar
 from .stirrup import Stirrup
